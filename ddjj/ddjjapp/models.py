3402a0b05276f4ff1295eb4ecb65f772f289e341
Gustavo Ajzenman <gustavoajz@gmail.com>
2013-09-12 14:29:39 -0300

    fix models

--- ddjj/ddjjapp/models.py
+++ ddjj/ddjjapp/models.py
@@ -14,20 +14,115 @@ class PreviousJob(models.Model):
     was_state_related = models.BooleanField()
 
 
+class AffidavitTemplate(models.Model):
+
+    """
+        El modelo de una declaración jurada.
+        Por ejemplo:
+            Las DDJJ de la Ciudad eran completadas a mano creo que hasta 2009.
+            Las DDJJ del Poder Judicial, tienen otras características.
+            Acá debería poner de qué juridiscción, poder, etc. y la fecha
+            o rango defechas.
+
+        Entonces se debería poder buscar: del Buenos Aires, el Poder Ejecutivo,
+        de tal a tal fecha, cuál es el template usado.
+    """
+
+    LEGISLATURE = 'L'
+    EXECUTIVE = 'E'
+    JUDICIARY = 'J'
+    POWER_CHOICES = (
+        (LEGISLATURE, 'Legislativo'),
+        (EXECUTIVE, 'Ejecutivo'),
+        (JUDICIARY, 'Judicial'),
+    )
+
+    power = models.CharField(
+        max_length=1, choices=POWER_CHOICES, default=EXECUTIVE)
+    jurisdiction = models.ForeignKey('Jurisdiction')
+    # El instructivo para completar.
+    sample_document = models.FileField(upload_to='documents')
+    init_date = models.DateField()
+    finish_date = models.DateField()
+
+
+class Document(models.Model):
+
+    """
+        Un documento scaneado con una declaración jurada.
+    """
+    document_file = models.FileField(upload_to='documents')
+    upload_date = models.DateTimeField()
+
+    # qué formato tiene la declaración jurada.
+    template = models.ForeignKey('AffidavitTemplate')
+
+    notes = models.TextField()
+
+
+class Affidavit(models.Model):
+
+    """
+        Una declaración jurada.
+    """
+    MARRIED = 'M'
+    SINGLE = 'S'
+    DIVORCED = 'D'
+    WIDOWER = 'W'
+
+    MARITAL_STATUS_CHOICES = (
+        (MARRIED, 'Casado'),
+        (SINGLE, 'Soltero'),
+        (DIVORCED, 'Divorciado'),
+        (WIDOWER, 'Viudo'),
+    )
+    # que a su vez tiene un template de donde se saca la jurisdicción y el
+    # poder.
+    original_document = models.OneToOneField(Document)
+
+    submission_date = models.DateTimeField()
+
+    # Datos personales que no cambian de una declaración jurada a otra (DNI,
+    # nombre, etc.)
+    person = models.ForeignKey('Person', )
+    marital_status = models.CharField(
+        max_length=1, choices=MARITAL_STATUS_CHOICES, default=MARRIED)
+
+    # Datos del cargo.
+    position_entry_date = models.DateField()
+    position = models.ForeignKey('Position')
+    # tipo de contrato, probablemente lo debería hacer CHOICES
+    contract_type = models.CharField(max_length=50)
+    anual_net_salary = models.DecimalField(
+        max_digits=12, decimal_places=2)
+
+    # Estudios
+    # REVIEW
+    studies = models.CharField(max_length=100)
+
+    spouse = models.ForeignKey('Person', related_name='+')
+    sons = models.ManyToManyField('Person', related_name='+')
+
+    voluntary_retirement = models.BooleanField()
+
+    worked_before_position = models.BooleanField()
+
+
 class Income(models.Model):
-    affidavit = models.ForeignKey(Affidavit)
+    affidavit = models.ForeignKey('Affidavit')
     ammount_year = models.DecimalField(
         max_digits=12, decimal_places=2)
     notes = models.TextField()
 
 
 class Property(PolymorphicModel):
-    percentage = models.DecimalField()
+    percentage = models.DecimalField(max_digits=3, decimal_places=2)
     buy_year = models.DateField()
 
-    bought_value = models.DecimalField()
+    bought_value = models.DecimalField(
+        max_digits=10, decimal_places=2)
 
-    affidavit = models.ForeignKey(Affidavit)
+    affidavit = models.ForeignKey('Affidavit')
 
     # REVIEW
     money_origin = models.CharField(max_length=100)
@@ -39,14 +134,17 @@ class FiscalProperty(Property):
         Propiedades con valor fiscal
     """
 
-    fiscal_value = models.DecimalField()    # Only valid value.
+    # Only valid value.
+    fiscal_value = models.DecimalField(
+        max_digits=10, decimal_places=2)
 
 
 class PaperProperty(Property):
 
     """
     """
-    current_value = models.DecimalField()
+    current_value = models.DecimalField(
+        max_digits=10, decimal_places=2)
 
 
 class RealState(FiscalProperty):
@@ -63,11 +161,13 @@ class RealState(FiscalProperty):
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
 
-    size = models.DecimalField()
+    size = models.DecimalField(max_digits=10, decimal_places=2)
 
-    unidad = models.DecimalField()  # no tengo idea de lo que es esto.
+    # no tengo idea de lo que es esto.
+    unidad = models.CharField(max_length=10)
 
-    improvements = models.DecimalField()
+    improvements = models.DecimalField(
+        max_digits=10, decimal_places=2)
 
 
 class CompanyShare(PaperProperty):
@@ -109,7 +209,7 @@ class BankAccount(models.Model):
     )
 
     property_type = models.CharField(
-        max_length=1, choices=PROPERTY_TYPE_CHOICES, default=STOCKS)
+        max_length=1, choices=ACCOUNT_TYPE_CHOICES, default=CHECKING_ACCOUNT)
     currency = models.CharField(max_length=3, default='ARS')
 
 
@@ -152,54 +252,8 @@ class Jurisdiction(models.Model):
             name: Bolivia
             parent: None
     """
-    name = models.CharType(max_length=50)
-    parent = ForeignKey(Jurisdiction)
-
-
-class AffidavitTemplate(models.Model):
-
-    """
-        El modelo de una declaración jurada.
-        Por ejemplo:
-            Las DDJJ de la Ciudad eran completadas a mano creo que hasta 2009.
-            Las DDJJ del Poder Judicial, tienen otras características.
-            Acá debería poner de qué juridiscción, poder, etc. y la fecha
-            o rango defechas.
-
-        Entonces se debería poder buscar: del Buenos Aires, el Poder Ejecutivo,
-        de tal a tal fecha, cuál es el template usado.
-    """
-
-    LEGISLATURE = 'L'
-    EXECUTIVE = 'E'
-    JUDICIARY = 'J'
-    POWER_CHOICES = (
-        (LEGISLATURE, 'Legislativo'),
-        (EXECUTIVE, 'Ejecutivo'),
-        (JUDICIARY, 'Judicial'),
-    )
-
-    power = models.CharField(
-        max_length=1, choices=POWER_CHOICES, default=EXECUTIVE)
-    jurisdiction = models.ForeignKey(Jurisdiction)
-    # El instructivo para completar.
-    sample_document = models.FileField(upload_to='documents')
-    init_date = models.DateField()
-    finish_date = models.DateField()
-
-
-class Document(models.Model):
-
-    """
-        Un documento scaneado con una declaración jurada.
-    """
-    document_file = models.FileField(upload_to='documents')
-    upload_date = models.DateTimeField()
-
-    # qué formato tiene la declaración jurada.
-    template = models.ForeignKey(AffidavitTemplate)
-
-    notes = models.TextField()
+    name = models.CharField(max_length=50)
+    parent = models.ForeignKey('Jurisdiction')
 
 
 class OtherActivities(models.Model):
@@ -209,56 +263,9 @@ class OtherActivities(models.Model):
     init_date = models.DateField()
     finish_date = models.DateField()
     leave = models.BooleanField()  # licencia
-    hours_per_week = models.DecimalField()
+    hours_per_week = models.DecimalField(
+        max_digits=3, decimal_places=2)
     state_related = models.BooleanField()
-
-
-class Affidavit(models.Model):
-
-    """
-        Una declaración jurada.
-    """
-    MARRIED = 'M'
-    SINGLE = 'S'
-    DIVORCED = 'D'
-    WIDOWER = 'W'
-
-    MARITAL_STATUS_CHOICES = (
-        (MARRIED, 'Casado'),
-        (SINGLE, 'Soltero'),
-        (DIVORCED, 'Divorciado'),
-        (WIDOWER, 'Viudo'),
-    )
-    # que a su vez tiene un template de donde se saca la jurisdicción y el
-    # poder.
-    original_document = models.OneToOneField(Document)
-
-    submission_date = models.DateTimeField()
-
-    # Datos personales que no cambian de una declaración jurada a otra (DNI,
-    # nombre, etc.)
-    person = models.ForeignKey(Person)
-    marital_status = models.CharField(
-        max_length=1, choices=MARITAL_STATUS_CHOICES, default=MARRIED)
-
-    # Datos del cargo.
-    position_entry_date = models.DateField()
-    position = models.ForeignKey(Position)
-    # tipo de contrato, probablemente lo debería hacer CHOICES
-    contract_type = models.CharField(max_length=50)
-    anual_net_salary = models.DecimalField(
-        max_digits=12, decimal_places=2)
-
-    # Estudios
-    # REVIEW
-    studies = models.CharField(max_length=100)
-
-    spouse = models.ForeifnKey(Person)
-    sons = models.ManyToMany(Person)
-
-    voluntary_retirement = models.BooleanField()
-
-    worked_before_position = models.BoolearField()
 
 
 class Person(models.Model):
@@ -269,14 +276,14 @@ class Person(models.Model):
     """
     DNI = 'D'
     ID_TYPE_CHOICES = (
-        (DNI, 'DNI')
+        (DNI, 'DNI'),
     )
 
     MALE = 'M'
     FEMALE = 'F'
     GENDER_CHOICES = (
         (MALE, 'Masculino'),
-        (FEMALE, 'Femenino')
+        (FEMALE, 'Femenino'),
     )
 
     id_type = models.CharField(
@@ -284,7 +291,7 @@ class Person(models.Model):
     id_number = models.CharField(max_length=15)
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    birth_date = DateField()
+    birth_date = models.DateField()
     gender = models.CharField(
         max_length=1, choices=GENDER_CHOICES, default=MALE)
     notes = models.TextField()
@@ -303,13 +310,13 @@ class Office(models.Model):
 
     """
     name = models.CharField(max_length=100)
-    parent = models.ForeignKey(Office)
+    parent = models.ForeignKey('Office')
 
     # Hay que pensar esto mejor: El problema es que cuando se crea un documento con un Document Template, no se sabe a que oficina corresponde.
     # Solamente se sabe que corresponde a una jurisdicción determinada.
     # La oficina se conoce cuando se crea la DDJJ, pero esa oficina tiene que tener una jurisdicción
     # Entonces, se crea una referencia circular.
-    jurisdiction = models.ForeignKey(Jurisdiction)
+    jurisdiction = models.ForeignKey('Jurisdiction')
 
 
 class Position(models.Model):
@@ -318,6 +325,6 @@ class Position(models.Model):
     Cargo
     """
     name = models.CharField(max_length=100)
-    office = models.ForeignKey(Office)
+    office = models.ForeignKey('Office')
 
     notes = models.TextField()
