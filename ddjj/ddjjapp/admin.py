7f331f8083ca47b706a3ba0cd91a71bad53815d4
Gustavo Ajzenman <gustavoajz@gmail.com>
2013-09-12 21:04:27 -0300

    added affidavit template to admin

--- ddjj/ddjjapp/admin.py
+++ ddjj/ddjjapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Document
+
+from .models import Document, AffidavitTemplate
 
 admin.site.register(Document)
+admin.site.register(AffidavitTemplate)
