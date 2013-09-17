1446df10d8445542c9f48cd804d023056a023109
Gustavo Ajzenman <gustavoajz@gmail.com>
2013-09-17 00:21:57 -0300

    add pyjadadd pyjadee

--- ddjj/ddjjsite/settings.py
+++ ddjj/ddjjsite/settings.py
@@ -95,9 +95,13 @@ SECRET_KEY = '&npg=95xsb^(xfy_x(+m7!41zy(s9lht73k+xk#h!1a)&pq)95'
 # List of callables that know how to import templates from various
 # sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-    #     'django.template.loaders.eggs.Loader',
+    ('pyjade.ext.django.Loader',
+        (
+     'django.template.loaders.filesystem.Loader',
+     'django.template.loaders.app_directories.Loader',
+     #     'django.template.loaders.eggs.Loader',
+     )
+     ),
 )
 
 MIDDLEWARE_CLASSES = (
