f10da588182f59cbcf7e08c65e4687dac36e1acb
Gustavo Ajzenman <gustavoajz@gmail.com>
2013-09-17 02:04:39 -0300

    add Person admin

--- ddjj/ddjjapp/admin.py
+++ ddjj/ddjjapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Document, AffidavitTemplate
+from .models import Document, AffidavitTemplate, Person
 
 admin.site.register(Document)
 admin.site.register(AffidavitTemplate)
+admin.site.register(Person)
